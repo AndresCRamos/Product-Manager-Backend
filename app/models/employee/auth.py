68951b09fb7064296c52f0f81b87084ac9ccd057
AndresCRamos <andrescramosr@gmail.com>
2021-06-23 19:48:04 -0500

    Change Login system

--- app/models/employee/auth.py
+++ app/models/employee/auth.py
@@ -3,73 +3,83 @@ from django.contrib.sessions.models import Session
 from django.contrib.auth.hashers import check_password
 from rest_framework import status
 from rest_framework.authtoken.models import Token
+from rest_framework.decorators import permission_classes, authentication_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from app.token_management import ExpiringTokenAuthentication
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from app.token_management import is_token_expired
 from .models import Employee
-from .serializers import EmployeeSerializer
+from .serializers import EmployeeSerializer, LoginSerializer
 
 
+@permission_classes([AllowAny])
 class Login(APIView):
     def post(self, request, *args, **kwargs):
-        email = request.data['email']
-        password = request.data['password']
-        try:
-            user = Employee.objects.get(email=email)
-        except Employee.DoesNotExist:
+        login_serializer = LoginSerializer(data=request.data)
+        if login_serializer.is_valid():
+            email = login_serializer.data['email']
+            password = login_serializer.data['password']
+            try:
+                user = Employee.objects.get(email=email)
+            except Employee.DoesNotExist:
+                return Response(
+                    {'error': 'No user which such email'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            if user:
+                pass_valid = check_password(password, user.password)
+                if not pass_valid:
+                    return Response(
+                        {'error': 'Incorrect password'},
+                        status=status.HTTP_400_BAD_REQUEST
+                    )
+                if user.is_active:
+                    token, created = Token.objects.get_or_create(user=user)
+                    employee = user
+                    employee_serializer = EmployeeSerializer(employee)
+                    if created:
+                        return Response(
+                            {
+                                'token': token.key,
+                                'employee': employee_serializer.data,
+                                'message': 'Token created'
+                            },
+                            status=status.HTTP_201_CREATED
+                        )
+                    else:
+                        token.delete()
+                        all_sessions = Session.objects.filter(expire_date__gte=datetime.now())
+                        if all_sessions.exists():
+                            for session in all_sessions:
+                                session_data = session.get_decoded()
+                                if user.id_card == int(session_data.get('_auth_user_id')):
+                                    session.delete()
+                        token = Token.objects.create(user=user)
+                        return Response(
+                            {
+                                'token': token.key,
+                                'employee': employee_serializer.data,
+                                'message': 'All sessions closed'
+                            },
+                            status=status.HTTP_201_CREATED
+                        )
+                return Response(
+                    {'message': "User isn't active"},
+                    status=status.HTTP_409_CONFLICT
+                )
             return Response(
                 {'error': 'No user which such email'},
                 status=status.HTTP_400_BAD_REQUEST
             )
-        if user:
-            pass_valid = check_password(password, user.password)
-            if not pass_valid:
-                return Response(
-                    {'error': 'Incorrect password'},
-                    status=status.HTTP_400_BAD_REQUEST
-                )
-            if user.is_active:
-                token, created = Token.objects.get_or_create(user=user)
-                employee = user
-                employee_serializer = EmployeeSerializer(employee)
-                if created:
-                    return Response(
-                        {
-                            'token': token.key,
-                            'employee': employee_serializer.data,
-                            'message': 'Token created'
-                        },
-                        status=status.HTTP_201_CREATED
-                    )
-                else:
-                    token.delete()
-                    all_sessions = Session.objects.filter(expire_date__gte=datetime.now())
-                    if all_sessions.exists():
-                        for session in all_sessions:
-                            session_data = session.get_decoded()
-                            if user.id_card == int(session_data.get('_auth_user_id')):
-                                session.delete()
-                    token = Token.objects.create(user=user)
-                    return Response(
-                        {
-                            'token': token.key,
-                            'employee': employee_serializer.data,
-                            'message': 'All sessions closed'
-                        },
-                        status=status.HTTP_201_CREATED
-                    )
-            return Response(
-                {'message': "User isn't active"},
-                status=status.HTTP_409_CONFLICT
-            )
-        return Response(
-            {'error': 'No user which such email'},
-            status=status.HTTP_400_BAD_REQUEST
-        )
+        return Response(login_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+@permission_classes([IsAuthenticated])
+@authentication_classes([ExpiringTokenAuthentication])
 class Logout(APIView):
     def get(self, request, *args, **kwargs):
-        token_sent = request.GET.get('token')
+        token_sent = request.META.get('HTTP_AUTHORIZATION').split()[1]
         if token_sent is None:
             return Response(
                 {'error': 'no token sent'},
@@ -78,12 +88,15 @@ class Logout(APIView):
         token = Token.objects.filter(key=token_sent).first()
         if token:
             user = token.user
+            print(user.id_card)
             all_sessions = Session.objects.filter(expire_date__gte=datetime.now())
             if all_sessions.exists():
                 for session in all_sessions:
-                    session_data = session.get_decoded()
-                    if user.id == int(session_data.get('_auth_user_id')):
-                        session.delete()
+                    if session:
+                        session_data = session.get_decoded()
+                        print('session id: ', session_data)
+                        if user.id_card == int(session_data.get('_auth_user_id')):
+                            session.delete()
             token.delete()
             session_message = 'User sessions deleted'
             token_message = 'Token deleted'
@@ -98,3 +111,27 @@ class Logout(APIView):
             {'message': 'no user with this credentials'},
             status=status.HTTP_400_BAD_REQUEST
         )
+
+
+@permission_classes([AllowAny])
+class RefreshToken(APIView):
+    def get(self, request, *args, **kwargs):
+        old_token = request.META.get('HTTP_AUTHORIZATION')
+        if old_token is None:
+            return Response({'error': 'no token was provided'}, status=status.HTTP_400_BAD_REQUEST)
+        old_token_key = old_token.split()[1]
+        try:
+            token = Token.objects.get(key=old_token_key)
+        except Token.DoesNotExist:
+            return Response({'error': 'No such Token'}, status=status.HTTP_400_BAD_REQUEST)
+        if token:
+            if is_token_expired(token):
+                token.delete()
+            new_token, created = Token.objects.get_or_create(user=token.user)
+            return Response(
+                {
+                    'token': new_token.key,
+                    'refreshed': created
+                }
+            )
+        return Response({'error': 'No such Token'}, status=status.HTTP_400_BAD_REQUEST)
